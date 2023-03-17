94222004a3f44582728bf05224611d314751c094
luizatorrens <luiza.torrens.2006@gmail.com>
2023-03-17 14:12:37 -0300

    Modificação no arquivo models.py

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora
+from .models import Categoria, Editora, Autor, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
 
 def __str__(self):
     return self.descricao
